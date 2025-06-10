74897c50e228e3e5c74f9e594a7b03f8dde7498d
Jose Barraza <jose.barraza@oracle.com>
2025-06-10 17:57:29 -0600

    Fix layouts

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,22 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
+
 import Layout from '../../components/Layout';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: 'Finance App',
-  description: 'Investments',
-};
 
 export default function RootLayout({
   children,
@@ -24,12 +9,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+  
         <Layout>{children}</Layout>
-      </body>
-    </html>
+     
   );
 }
